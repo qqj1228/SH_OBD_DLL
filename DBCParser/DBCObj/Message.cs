2cb7286e059492ead517fd12826b8c9d10535a2e
qqj1228 <qqj1228@gmail.com>
2020-11-30 11:49:20 +0800

    added DBCParser

---
+++ DBCParser/DBCObj/Message.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DBCParser.DBCObj {
+    public class Message : DBCObjBase {
+        public Dictionary<string, Signal> Signals { get; private set; }
+        public uint ID { get; set; }
+        public string Name { get; set; }
+        public int Length { get; set; } // 以字节为单位
+        public Node Sender { get; set; }
+
+        public Message(uint ID, string name, int length) {
+            Signals = new Dictionary<string, Signal>();
+            this.ID = ID;
+            this.Name = name;
+            this.Length = length;
+        }
+
+        public void AddSignal(Signal signalIn) {
+            // 特殊ID表示该报文作为未使用信号的容器，不必检查新信号的合法性
+            if (ID != 0xC0000000) {
+                if (signalIn.Message != null) {
+                    throw new ApplicationException("Signal already belongs to message: " + signalIn.Message.Name);
+                }
+                if (!CheckSignalPosition(signalIn)) {
+                    throw new ApplicationException("Signal position doesn't fit in message layout");
+                }
+            }
+            AddChild(signalIn);
+            Signals.Add(signalIn.Name, signalIn);
+        }
+
+        /// <summary>
+        /// 简单检查新信号在报文中的位置是否合法
+        /// </summary>
+        /// <param name="signalIn"></param>
+        /// <returns></returns>
+        private bool CheckSignalPosition(Signal signalIn) {
+            List<int[]> posListIn = signalIn.GetRawPosition();
+            if (posListIn[posListIn.Count - 1][1] >= this.Length * 8) {
+                return false;
+            }
+            // 若是扩展复用信号的话，SG_MUL_VAL_会在文件最后才读入，故无法判断其位置是否正确
+            //foreach (Signal signal in Signals.Values) {
+            //    List<int[]> posList = signal.GetRawPosition();
+            //    if (signal.MultiplexedID == signalIn.MultiplexedID || signal.MultiplexedID < 0 || signalIn.MultiplexedID < 0) {
+            //        foreach (int[] pos in posList) {
+            //            foreach (int[] posIn in posListIn) {
+            //                if (!(posIn[1] < pos[0] || pos[1] < posIn[0])) {
+            //                    return false;
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
+            return true;
+        }
+
+        /// <summary>
+        /// 加载完dbc文件后，对报文的后处理函数。目前仅包含对信号的处理
+        /// </summary>
+        public void PostProcess() {
+            List<Signal> MUXors = new List<Signal>();
+            foreach (Signal signal in Signals.Values) {
+                if (signal.Multiplexor) {
+                    MUXors.Add(signal);
+                }
+            }
+            foreach (Signal signal in Signals.Values) {
+                if (signal.MultiplexedID >= 0 && (signal.MultiplexorValue.Key == null)) {
+                    int[] iIDs = new int[] { signal.MultiplexedID, signal.MultiplexedID };
+                    List<int[]> MUXedIDs = new List<int[]> { iIDs };
+                    signal.MultiplexorValue = new KeyValuePair<Signal, List<int[]>>(MUXors[0], MUXedIDs);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 用于dbc文件读入后的纯数据解析，必须在dbc文件全部解析完成后才能执行
+        /// </summary>
+        /// <param name="strData"></param>
+        /// <returns></returns>
+        public bool SetSignalRawValue(string strData) {
+            // 判断输入的数据长度是否正确
+            bool wrongLen = true;
+            if (Attributes.ContainsKey("DLCMin")) {
+                DBCAttribute attribute = Attributes["DLCMin"];
+                if (attribute.AttriValue.iValue != 0) {
+                    if (strData.Length >= attribute.AttriValue.iValue * 2) {
+                        wrongLen = false;
+                    }
+                }
+            }
+            if (strData.Length < Length * 2 && wrongLen) {
+                return false;
+            }
+
+            // 计算信号原始数值
+            foreach (Signal signal in Signals.Values) {
+                List<int[]> rawPos = signal.GetRawPosition(); // 信号在报文中的原始位置List
+                List<byte> parts = new List<byte>(); // 将输入数据的字符串转成16进制后得到的字节List
+                foreach (int[] pos in rawPos) {
+                    int index = pos[0] / 8;
+                    if (index * 2 + 1 < strData.Length) {
+                        string strPart = strData.Substring(index * 2, 2);
+                        byte part = Convert.ToByte(strPart, 16);
+                        part = (byte)(part << (7 - pos[1] % 8));
+                        part = (byte)(part >> (7 - pos[1] % 8 + pos[0] % 8));
+                        parts.Add(part);
+                    }
+                }
+                if (parts.Count == 0) {
+                    continue;
+                }
+
+                signal.ListString.Clear();
+                if (signal.Unit == "ASCII") {
+                    if (signal.Length > 8) {
+                        string strVal = string.Empty;
+                        foreach (byte part in parts) {
+                            if (0x20 <= part && part <= 0x7E) {
+                                strVal += Convert.ToChar(part);
+                            }
+                        }
+                        signal.ListString.Add(strVal);
+                    } else if (signal.Length == 8) {
+                        int index = rawPos[0][0] / 8;
+                        string strVal = HexStringToASCIIString(index, Length, strData);
+                        signal.ListString.Add(strVal);
+                        if (Attributes.ContainsKey("MultipleItem")) {
+                            DBCAttribute attribute = Attributes["MultipleItem"];
+                            if (attribute.AttriValue.iValue > 0) {
+                                int start = Length;
+                                int scale = 2;
+                                while (strData.Length / 2 >= Length * scale) {
+                                    strVal = HexStringToASCIIString(start, Length * scale, strData);
+                                    signal.ListString.Add(strVal);
+                                    start += Length;
+                                    ++scale;
+                                }
+                            }
+                        }
+                    } else {
+                        return false;
+                    }
+                } else if (signal.Unit == "HEX") {
+                    string strVal = strData.Substring(0, Length * 2);
+                    signal.ListString.Add(strVal);
+                    if (Attributes.ContainsKey("MultipleItem")) {
+                        DBCAttribute attribute = Attributes["MultipleItem"];
+                        if (attribute.AttriValue.iValue > 0) {
+                            int start = Length;
+                            int scale = 2;
+                            while (strData.Length / 2 >= Length * scale) {
+                                strVal = strData.Substring(start * 2, Length * 2);
+                                signal.ListString.Add(strVal);
+                                start += Length;
+                                ++scale;
+                            }
+                        }
+                    }
+                } else {
+                    uint rawValue = parts[0];
+                    for (int i = 1; i < parts.Count; i++) {
+                        rawValue <<= rawPos[i][1] - rawPos[i][0] + 1;
+                        rawValue += parts[i];
+                    }
+                    if (signal.Signed) {
+                        signal.RawValue = (int)rawValue;
+                    } else {
+                        signal.RawValue = rawValue;
+                    }
+                }
+            }
+            // 根据信号的直接MUXor判断信号在报文中是否被使用
+            // 并不判断MUXor是否也可用（目前阶段无法判断，需全部信号处理完后才行）
+            foreach (Signal signal in Signals.Values) {
+                if (signal.MultiplexedID < 0) {
+                    signal.UsedInMessage = true;
+                } else {
+                    Signal MUXor = signal.MultiplexorValue.Key;
+                    foreach (int[] MUXedIDs in signal.MultiplexorValue.Value) {
+                        if (MUXedIDs[0] <= MUXor.RawValue && MUXor.RawValue <= MUXedIDs[1]) {
+                            signal.UsedInMessage = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+        private string HexStringToASCIIString(int start, int length, string strData) {
+            string strVal = string.Empty;
+            for (int i = start; i < length; i++) {
+                string strPart = strData.Substring(i * 2, 2);
+                byte part = Convert.ToByte(strPart, 16);
+                if (0x20 <= part && part <= 0x7E) {
+                    strVal += Convert.ToChar(part);
+                }
+            }
+            return strVal;
+        }
+    }
+}
---
+++ DBCParser/DBCObj/Signal.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+
+namespace DBCParser.DBCObj {
+    public class Signal : DBCObjBase {
+        public string Name { get; set; }
+        public int StartBit { get; set; }
+        public int Length { get; set; } // 以bit为单位
+        public bool IntelOrder { get; set; }
+        public bool Signed { get; set; }
+        public double Factor { get; set; }
+        public double Offset { get; set; }
+        public double ValueMin { get; set; }
+        public double ValueMax { get; set; }
+        public string Unit { get; set; }
+        public bool Multiplexor { get; set; } // 是否为复用开关信号
+        public int MultiplexedID { get; set; } // 为负值时表示不是复用信号
+        public List<Node> Receiver { get; private set; }
+        public Message Message { get; set; }
+        public Dictionary<uint, string> ValueDescs { get; set; }
+        public KeyValuePair<Signal, List<int[]>> MultiplexorValue { get; set; } // 扩展复用开关信号值，key：该信号对应的扩展复用开关信号
+        public bool UsedInMessage { get; set; } // 在报文中是否被使用
+        public List<string> ListString { get; set; } // 存放信号中的ASCII字符串List
+        public string DisplayString { get; set; } // 在报文解析后存放用于对外显示的字符串
+
+        private double _rawValue;
+        /// <summary>
+        /// 未转换为物理值的原始数值
+        /// </summary>
+        public double RawValue {
+            get { return _rawValue; }
+            set {
+                if (!this.Signed && value < 0) {
+                    throw new ApplicationException("Signed value not allowed");
+                }
+                int usableLen = this.Signed ? this.Length - 1 : this.Length;
+                if (Math.Pow(2, usableLen) <= Math.Abs(value)) {
+                    throw new ApplicationException("Value exceeds signal length");
+                }
+                _rawValue = value;
+            }
+        }
+
+        /// <summary>
+        /// 将 raw value 转换为实际的物理值
+        /// </summary>
+        public double Value {
+            get {
+                return this.RawValue * this.Factor + this.Offset;
+            }
+            set {
+                double tempV = value;
+                if (ValueMin != 0 || ValueMax != 0) {
+                    tempV = Math.Max(tempV, ValueMin);
+                    tempV = Math.Min(tempV, ValueMax);
+                }
+                _rawValue = (tempV - this.Offset) / this.Factor;
+            }
+        }
+
+        /// <summary>
+        /// 信号类构造函数
+        /// bMultiplexer为true，multiplexerID为-1的话，该信号为复用信号交换机，表示报文中不变的部分
+        /// bMultiplexer为true，multiplexerID为大于等于0的值的话，该信号为复用信号，
+        /// 不同的ID值表示不同的信号复用情况，相同的ID表示在同一个报文中
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="startBit"></param>
+        /// <param name="length"></param>
+        /// <param name="bIntelOrder"></param>
+        /// <param name="bSigned"></param>
+        /// <param name="factor"></param>
+        /// <param name="offset"></param>
+        /// <param name="valueMin"></param>
+        /// <param name="valueMax"></param>
+        /// <param name="unit"></param>
+        /// <param name="bMultiplexor"></param>
+        /// <param name="iMultiplexedID"></param>
+        public Signal(string name, int startBit, int length, bool bIntelOrder = false, bool bSigned = false,
+            double factor = 1.0, double offset = 0.0, double valueMin = 0, double valueMax = 0,
+            string unit = "", bool bMultiplexor = false, int iMultiplexedID = -1) {
+            Name = name;
+            StartBit = startBit;
+            Length = length;
+            IntelOrder = bIntelOrder;
+            Signed = bSigned;
+            Factor = factor;
+            Offset = offset;
+            ValueMin = valueMin;
+            ValueMax = valueMax;
+            Unit = unit;
+            Multiplexor = bMultiplexor;
+            MultiplexedID = iMultiplexedID;
+
+            Receiver = new List<Node>();
+            Message = null;
+            ValueDescs = new Dictionary<uint, string>();
+            MultiplexorValue = new KeyValuePair<Signal, List<int[]>>();
+            UsedInMessage = false;
+            ListString = new List<string>();
+            _rawValue = 0;
+        }
+
+        public void AddReceiver(Node node) {
+            Receiver.Add(node);
+        }
+
+        /// <summary>
+        /// 返回信号在报文中的原始起止bit位置[start, end]数组的List，
+        /// List的大小即为该信号占用报文的字节数，
+        /// bit位置从小到大排列，该值已考虑字节顺序，使用时无需再计算，
+        /// </summary>
+        public List<int[]> GetRawPosition() {
+            List<int[]> rets = new List<int[]>();
+
+            int rest = Length;
+            if (IntelOrder) {
+                int lsb = StartBit;
+                while (rest > 0) {
+                    int[] pos = new int[2];
+                    pos[0] = lsb;
+                    pos[1] = (8 * ((lsb + 8) / 8)) - 1;
+                    if (rest - (pos[1] - pos[0] + 1) < 0) {
+                        pos[1] = pos[0] + rest - 1;
+                    }
+                    rets.Add(pos);
+                    rest -= (pos[1] - pos[0] + 1);
+                    lsb = pos[1] + 1;
+                }
+            } else {
+                int msb = StartBit;
+                while (rest > 0) {
+                    int[] pos = new int[2];
+                    pos[1] = msb;
+                    int a = msb - 8;
+                    if (a < 0) {
+                        pos[0] = 0;
+                    } else {
+                        pos[0] = 8 * (a / 8 + 1);
+                    }
+                    if (rest - (pos[1] - pos[0] + 1) < 0) {
+                        pos[0] = msb - rest + 1;
+                    }
+                    rets.Add(pos);
+                    rest -= (pos[1] - pos[0] + 1);
+                    msb = pos[0] + 15;
+                }
+            }
+            return rets;
+        }
+
+        /// <summary>
+        /// 判断信号是否可用，必须在dbc文件全部解析完成后才能执行，
+        /// 返回值 > 0 : 可用，返回值 <= 0 : 不可用，0 - 由当前信号的MUXor判断不可用，-1 - 由上一层MUXor判断不可用，其余负值依次类推
+        /// </summary>
+        /// <param name="sigIn"></param>
+        /// <returns></returns>
+        public int TestSignalUesed() {
+            int iUsed;
+            if (UsedInMessage) {
+                iUsed = 1;
+            } else {
+                iUsed = 0;
+            }
+            if (MultiplexorValue.Key == null) {
+                return iUsed;
+            } else {
+                int iMUXor = MultiplexorValue.Key.TestSignalUesed();
+                if (iMUXor <= 0) {
+                    return iMUXor - 1;
+                } else {
+                    return iUsed;
+                }
+            }
+        }
+
+    }
+}
